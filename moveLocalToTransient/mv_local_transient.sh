4e885b325bbb2ca7c955768ef3f4da8a162b2c72
telilingan <telilingan@gmail.com>
2018-05-25 11:31:05 +0700

    Added time information

--- moveLocalToTransient/mv_local_transient.sh
+++ moveLocalToTransient/mv_local_transient.sh
@@ -16,14 +16,14 @@ fMOVE() {
 	DAYSR=$(echo $i | cut -c 49-50)
 
 	if [ $PROBES != "rob" ]; then
-		echo "hadoop fs -mkdir -p /data/transient/url_${PROBES}_${YEARS}${MONTHS}/ds=$YEARS-$MONTHS-$DAYS"
+		echo "`date +%Y-%m-%dT%H:%M:%S` hadoop fs -mkdir -p /data/transient/url_${PROBES}_${YEARS}${MONTHS}/ds=$YEARS-$MONTHS-$DAYS"
 		hadoop fs -mkdir -p /data/transient/url_${PROBES}_${YEARS}${MONTHS}/ds=$YEARS-$MONTHS-$DAYS
-		echo "hadoop fs -moveFromLocal $i /data/transient/url_${PROBES}_${YEARS}${MONTHS}/ds=$YEARS-$MONTHS-$DAYS"
+		echo "`date +%Y-%m-%dT%H:%M:%S` hadoop fs -moveFromLocal $i /data/transient/url_${PROBES}_${YEARS}${MONTHS}/ds=$YEARS-$MONTHS-$DAYS"
 		hadoop fs -moveFromLocal $i /data/transient/url_${PROBES}_${YEARS}${MONTHS}/ds=$YEARS-$MONTHS-$DAYS
 	else
-		echo "hadoop fs -mkdir -p /data/transient/url_${PROBESR}_${YEARSR}${MONTHSR}/ds=$YEARSR-$MONTHSR-$DAYSR"
+		echo "`date +%Y-%m-%dT%H:%M:%S` hadoop fs -mkdir -p /data/transient/url_${PROBESR}_${YEARSR}${MONTHSR}/ds=$YEARSR-$MONTHSR-$DAYSR"
 		hadoop fs -mkdir -p /data/transient/url_${PROBESR}_${YEARSR}${MONTHSR}/ds=$YEARSR-$MONTHSR-$DAYSR
-		echo "hadoop fs -moveFromLocal $i /data/transient/url_${PROBESR}_${YEARSR}${MONTHSR}/ds=$YEARSR-$MONTHSR-$DAYSR"
+		echo "`date +%Y-%m-%dT%H:%M:%S` hadoop fs -moveFromLocal $i /data/transient/url_${PROBESR}_${YEARSR}${MONTHSR}/ds=$YEARSR-$MONTHSR-$DAYSR"
 		hadoop fs -moveFromLocal $i /data/transient/url_${PROBESR}_${YEARSR}${MONTHSR}/ds=$YEARSR-$MONTHSR-$DAYSR
 	fi
 }
